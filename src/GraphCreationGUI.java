38e2e1841d51408930fbbe2a47a5286e2a412171
Timmae <tim-timmae@gmx.net>
2013-06-07 16:21:59 +0200

    integrated the jung framework to be able to create graphs

--- src/GraphCreationGUI.java
+++ src/GraphCreationGUI.java
@@ -18,18 +18,42 @@ import java.awt.event.ComponentListener;
 
 public class GraphCreationGUI extends JFrame {
 
-	private JComboBox cb_graphType = new JComboBox();
-	private JComboBox cb_algorithm = new JComboBox();
-	JButton btn_visualisation = new JButton("Visualisation");
-	private JPanel pnl_graph = new JPanel();
-	private JFrame window = this;
-	private JPanel contentPane;
+	private JComboBox mCb_graphType = new JComboBox();
+	private JComboBox mCb_algorithm = new JComboBox();
+	private JButton mBtn_visualisation = new JButton("Visualisation");
+	private JPanel mPnl_graph = new JPanel();
+	private JFrame mWindow = this;
+	private JPanel mContentPane;
+	
+	private GraphBuilder mGraphBuilder;
 
+	/**
+	 * initializes the components
+	 */
+	private void initComponents() {
+		mContentPane = new JPanel();
+		mContentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(mContentPane);
+		mContentPane.setLayout(null);
+		
+		mCb_graphType.setBackground(Color.WHITE);
+		mContentPane.add(mCb_graphType);
+		
+		mCb_algorithm.setBackground(Color.WHITE);
+		mContentPane.add(mCb_algorithm);
+		
+		mPnl_graph.setBackground(Color.WHITE);
+		mContentPane.add(mPnl_graph);
+		
+		mContentPane.add(mBtn_visualisation);
+	}
+	
 	private ComponentListener resizeListener = new ComponentListener() {
 		@Override
 		public void componentResized(ComponentEvent e) {
 			System.out.println("resized!!");
 			resizeComponents();
+			mGraphBuilder.onResizePanel(mPnl_graph);
 		}
 
 		@Override
@@ -43,34 +67,37 @@ public class GraphCreationGUI extends JFrame {
 		}
 	};
 	
-	
+	/**
+	 * resize the components shown by the JFrame
+	 */
 	private void resizeComponents() {
-		Rectangle windowRect = contentPane.getBounds();
+		// get the rectangle of the contentPane
+		Rectangle windowRect = mContentPane.getBounds();
 		
+		// for each component: align the component relative to the contentPane rectangle and the other components
 		int x = 10;
 		int y = 10;
 		int w = 153;
 		int h = 24;
-		cb_graphType.setBounds(x, y, w, h);
+		mCb_graphType.setBounds(x, y, w, h);
 		
 		w = 150;
 		h = 25;
 		x = windowRect.width - w - 10;
 		y = windowRect.y + 10;
-		cb_algorithm.setBounds(x, y, w, h);
+		mCb_algorithm.setBounds(x, y, w, h);
 		
-		btn_visualisation.setBounds(304, 236, 126, 25);
-		w = 125;
+		w = 160;
 		h = 25;
 		x = windowRect.width - w - 10;
 		y = windowRect.height - h - 10;
-		btn_visualisation.setBounds(x, y, w, h);
+		mBtn_visualisation.setBounds(x, y, w, h);
 		
 		x = windowRect.x + 10;
-		y = cb_graphType.getBounds().y + cb_graphType.getBounds().height + 10;
+		y = mCb_graphType.getBounds().y + mCb_graphType.getBounds().height + 10;
 		w = windowRect.width - 20;
-		h = btn_visualisation.getBounds().y - 10 - cb_graphType.getBounds().y - cb_graphType.getBounds().height - 10;
-		pnl_graph.setBounds(x, y, w, h);
+		h = mBtn_visualisation.getBounds().y - 10 - mCb_graphType.getBounds().y - mCb_graphType.getBounds().height - 10;
+		mPnl_graph.setBounds(x, y, w, h);
 	}
 
 	/**
@@ -95,28 +122,15 @@ public class GraphCreationGUI extends JFrame {
 	public GraphCreationGUI() {
 		setTitle("ViGrAl - Graph Creation");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
 		setMinimumSize(new Dimension(460, 310));
 		addComponentListener(resizeListener);
 		
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		cb_graphType.setBackground(Color.WHITE);
-		contentPane.add(cb_graphType);
-		
-		cb_algorithm.setBackground(Color.WHITE);
-		contentPane.add(cb_algorithm);
-		
-		pnl_graph.setBackground(Color.WHITE);
-		contentPane.add(pnl_graph);
-		
-		contentPane.add(btn_visualisation);
-		
+		initComponents();
 		resizeComponents();
+		
+		setBounds(100, 100, 450, 300);
+		
+		mGraphBuilder = new GraphBuilder(mPnl_graph);
 	}
 	
 	
