facfaaf4f7885d39eca928a6836c82029b8d7253
chiller87 <tim-timmae@gmx.net>
2013-06-10 17:13:54 +0200

    added an onClose handler to the edit properties dialog, that refreshes the view

--- src/ElementPopupMenu.java
+++ src/ElementPopupMenu.java
@@ -1,5 +1,8 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.awt.geom.Point2D;
 
 import javax.swing.JFrame;
@@ -61,6 +64,13 @@ public class ElementPopupMenu {
 		    			dialog = new ElementPropertyDialog(frame, mVertex);
 					else // edge menu is called
 						dialog = new ElementPropertyDialog(frame, mEdge);
+					
+					dialog.addWindowListener(new WindowAdapter() {
+						@Override
+					    public void windowClosed(WindowEvent e) {
+					        mVViewer.repaint();
+					    }
+					});
 		            
 		            dialog.setVisible(true);
 		        }
