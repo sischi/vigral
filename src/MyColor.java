bf626340d929e07c0e67e333355a38a8f7b02629
chiller87 <tim-timmae@gmx.net>
2013-06-17 16:16:00 +0200

    added new class for color declaration

---
+++ src/MyColor.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+import java.awt.Paint;
+
+
+public class MyColor {
+
+	final public static Paint LIGHT_GREEN = new Color(0x99CC00);
+	final public static Paint LIGHT_GRAY = new Color(0xC4C4C4);
+	final public static Paint LIGHT_CYAN = new Color(0x33B5E5);
+	final public static Paint LIGHT_ORANGE = new Color(0xFFBB33);
+	final public static Paint YELLOW = Color.yellow;
+	
+}
